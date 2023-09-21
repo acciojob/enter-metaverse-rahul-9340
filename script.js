1ba11d69d8050a80b1d50906a35ec91ee439b972
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-09-21 17:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let para = document.getElementById("status")
+let button = document.getElementById("enterBtn");
+
+button.addEventListener("click", function(){
+   para.innerHTML = "<h1>Entered Metaverse</h1>";
+});
+
+
+
+
+
+
+
