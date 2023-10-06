fcc93481e5d9a6dc5dea2bcc46c9b1d3a71026c5
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-06 15:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ let button = document.getElementById("enterBtn");
 button.addEventListener("click", () => {
 let h1 = document.createElement("h1");
 	h1.innerText = "Entered Metaverse";
-	document.body.insertBefore(h1,para);
-     para.remove();
+	// para.remove();
+	para.innerHTML = "";
+	para.appendChild(h1)
+	// document.body.insertBefore(h1,para);
+     
 });
 
 
